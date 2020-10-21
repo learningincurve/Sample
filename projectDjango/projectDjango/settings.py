04cb801b2b0ae6b0e2dedbc5e15ba1c5c2abc235
mahesh <sudo.mahesh@gmail.com>
2020-10-22 00:37:37 +0530

    changes in project

--- projectDjango/projectDjango/settings.py
+++ projectDjango/projectDjango/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'projectDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER':  'dbadmin',
+        'PASSWORD': 'admin@123',
+        'HOST':'db',
+        'PORT': 3402,
     }
 }
 
